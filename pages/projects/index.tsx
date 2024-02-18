e51531132bc62ad4fd38721bcff2ef75d0398025
Sean <sfg.newell@gmail.com>
2024-02-18 13:05:20 +0000

    updating projects

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -49,29 +49,44 @@ const Content = () => {
       </div>
       <div className="flex w-full flex-col items-center gap-10 pt-10">
         <Project
+          id="axios"
+          title="Axios Helper Library"
+          subtitle="Helpful Wrapper Functions for Axios"
+          tags={['JavaScript', 'TypeScript']}
+          links={{
+            github: 'https://github.com/beritani/axios',
+            npm: 'https://www.npmjs.com/package/@beritani/axios',
+          }}
+        />
+
+        <Project
+          id="moneroj"
+          title="Moneroj"
+          subtitle="Monero Wallet Library written in JavaScript"
+          tags={['Cryptocurrency', 'JavaScript', 'TypeScript']}
+          links={{
+            github: 'https://github.com/beritani/moneroj',
+            npm: 'https://www.npmjs.com/package/moneroj',
+          }}
+        />
+
+        <Project
+          id="ring-signatures"
+          title="Ring Signatures"
+          subtitle="JavaScript Library for Creating and Verifying Ring Signatures"
+          tags={['Cryptography', 'JavaScript', 'TypeScript']}
+          links={{
+            github: 'https://github.com/beritani/ring-signatures',
+            npm: 'https://www.npmjs.com/package/ring-signatures',
+          }}
+        />
+
+        <Project
           id="whitebox"
           title="Whitebox"
           subtitle="Secure File Store Designed for Disaster Recovery"
-          tags={['Cryptography', 'TypeScript']}
-          links={{ github: 'https://github.com/potofdata/whitebox' }}
-        />
-
-        <Project
-          id="proflows"
-          title="Proflows"
-          subtitle="Open Source Low Code Data Pipeline"
-          tags={['Data Science', 'TypeScript']}
-          links={{ github: 'https://github.com/potofdata/proflows' }}
-        />
-
-        <Project
-          id="block-explorer"
-          title="Blockchain Explorer"
-          subtitle="Visualise and Explorer Cryptocurrency Blockchains"
-          tags={['Cryptocurrency', 'React', 'API', 'TypeScript']}
-          links={{
-            github: 'https://github.com/beritani/axios',
-          }}
+          tags={['Cryptography', 'Golang']}
+          links={{ github: 'https://github.com/beritani/whitebox' }}
         />
 
         <Project
@@ -80,17 +95,6 @@ const Content = () => {
           subtitle="Portfolio Website"
           tags={['NextJS', 'Tailwind', 'TypeScript']}
           links={{ github: 'https://github.com/beritani/seann.co.uk' }}
-        />
-
-        <Project
-          id="axios"
-          title="Axios Helper Library"
-          subtitle="Useful Wrapper Classes for Axios (HTTP Library)"
-          tags={['JavaScript', 'TypeScript']}
-          links={{
-            github: 'https://github.com/beritani/axios',
-            npm: 'https://www.npmjs.com/package/@beritani/axios',
-          }}
         />
       </div>
     </div>
